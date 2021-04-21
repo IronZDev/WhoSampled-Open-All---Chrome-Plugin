97adb839ac1760c41e05a5a64b9061d9c21e9d76
IronZaphilith <maciek_st@o2.pl>
2021-04-21 19:30:18 +0100

    Added handling for opening all samples from the details page

--- contentScript.js
+++ contentScript.js
@@ -1,18 +1,24 @@
 const songsOnPage = 16;
+const detailedPageRegex = /.*whosampled.com\/\S*\/\S+\/\S+\/$/;
+
+
+function OpenLinks(songURLs, songName, artistName) {
+	chrome.runtime.sendMessage({links: songURLs, song: songName, artist: artistName}, function(response) {
+		console.log(response.status);
+		if (response.status != "OK") {
+		  console.log(response.message);
+		}
+	});
+}
 
 function JustOpenVisible(section, songName, artistName) {
 	let songsToOpen = section.querySelectorAll('div.list > div.listEntry > a');
 	songsToOpen = [...songsToOpen].map(link => link.href);
-	chrome.runtime.sendMessage({links: songsToOpen, song: songName, artist: artistName}, function(response) {
-	  console.log(response.status);
-	  if (response.status != "OK") {
-		console.log(response.message);
-	  }
-	});
+	OpenLinks(songsToOpen, songName, artistName);
 	console.log(songsToOpen);
 }
 
-async function FetchAndOpenAll (url, songName, artistName) {
+async function FetchAndOpenAll (url, songName, artistName, sectionTitle) {
 	let songURLs = [];
 	// Naive implementation, fetch one page after another
 	await fetch(url).then(response => response.text())
@@ -20,7 +26,7 @@ async function FetchAndOpenAll (url, songName, artistName) {
 		const parser = new DOMParser();
 		const htmlDocument = parser.parseFromString(text, "text/html");
         console.log(htmlDocument.querySelectorAll("div.list > div.listEntry > a"));
-		songURLs.push([...htmlDocument.querySelectorAll("div.list > div.listEntry > a")].map(link => link.href));
+		songURLs = [...songURLs, ...[...htmlDocument.querySelectorAll("div.list > div.listEntry > a")].map(link => link.href)];
 		const nextBtn = htmlDocument.querySelector('div.pagination > span.next > a');
 		if (nextBtn) {
 			console.log(nextBtn.href);
@@ -28,7 +34,8 @@ async function FetchAndOpenAll (url, songName, artistName) {
 			songURLs.push(songsFromNextPage);
 		}
 	});
-	return songURLs;
+	console.log(songURLs);
+	OpenLinks(songURLs, songName, artistName);
 }
 
 async function OpenAll(btn) {
@@ -38,7 +45,12 @@ async function OpenAll(btn) {
 	const songName = document.querySelector('div.trackInfo > h1 > meta[itemprop="name"]').content;
 	const artistName = document.querySelector('div.trackInfo > h1 > span.trackArtistNames > meta[itemprop="name"]').content;
 	if (!seeAllBtn) {
-		JustOpenVisible(section, songName, artistName);
+		
+		if (detailedPageRegex.test(window.location.href)) {
+			await FetchAndOpenAll(window.location.href, songName, artistName, sectionTitle);
+		} else {
+			JustOpenVisible(section, songName, artistName);
+		}
 	} else {
 		await FetchAndOpenAll(seeAllBtn.href, songName, artistName, sectionTitle);
 	}
